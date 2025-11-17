fe750fe1f691443f8ad4e0134b84b90102fed328
Abhishek20042003 <141939848+Abhishek20042003@users.noreply.github.com>
2025-11-17 23:46:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function removecolor(e){
+	e.preventDefault()
+const drop = document.querySelector("#colorSelect")
+const index = drop.selectedIndex
+
+if(index!= -1){
+drop.remove(index)}
+}
+
+const input = document.querySelector("input")
+input.addEventListener("click", removecolor)
+
